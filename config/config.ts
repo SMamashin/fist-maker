74cd633da89d6121cb87cc28814aadfd615e6bb7
Stepan <107657774+SMamashin@users.noreply.github.com>
2024-01-13 19:21:28 +0300

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,7 @@
 export const token = 'vk-token';
+
+export const squareSize = 28; // 25-27
+export const squareRadius = 35; // 30-34
+export const squareColor = 'black';
+export const roundColor = 'black';
+export const roundSize = 10;
